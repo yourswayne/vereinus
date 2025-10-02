abfd2a9cbd18289b05cee6eb23e3e9e8a8f11c82
Facebadhat <waynevincent2005@gmail.com>
2025-10-02 13:26:10 +0200

    Projekt Reset, Native Tabs & App Icon
    Projekt zurückgesetzt. Am Tab Bar gearbeitet und App Icon hinzugefügt.

--- Vereinus/app/_layout.tsx
+++ Vereinus/app/_layout.tsx
@@ -1,24 +1,30 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Stack } from "expo-router";
+import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+
+   <NativeTabs>
+        <NativeTabs.Trigger name="news">
+        <Icon sf="bell.fill" />
+        <Label>Neuigkeiten</Label>
+      </NativeTabs.Trigger>
+    <NativeTabs.Trigger name="calender">
+        <Icon sf="calendar" />
+        <Label>Kalender</Label>
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="index">
+        <Label>Verein</Label>
+        <Icon sf="house.fill"  />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="tasklist">
+        <Icon sf="text.badge.checkmark" />
+        <Label>Aufgaben</Label>
+      </NativeTabs.Trigger>
+            <NativeTabs.Trigger name="setting">
+        <Icon sf="gear" />
+        <Label>Einstellung</Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
---
+++ Vereinus/app/setting.tsx
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Tab() {
+  return (
+    <View style={styles.container}>
+      <Text>Hier kommt die Einstellungen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
